34e93bcec7a4113385eae0256992d418c474e7d9
Zoe Laventhol <zoe.laventhol@gmail.com>
2022-10-26 13:37:04 +0200

    more react router setup

--- client/src/App.js
+++ client/src/App.js
@@ -2,24 +2,21 @@
 import logo from './logo.svg';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
+import FeaturedIndicator from './components/FeaturedIndicator.js';
+import Form from './components/Form.js';
+import IconGrid from './components/IconGrid.js';
+import About from './components/About.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      
+      <Routes>
+        {/* can pass props here */}
+        <Route path="/" element={<HomeView />} />
+        <Route path="/about" element={<About />} /> 
+      </Routes>
     </div>
   );
 }
