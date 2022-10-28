411b75e81620da47b04d0b0ba6ec5c53d47e95f9
Zoe Laventhol <zoe.laventhol@gmail.com>
2022-10-28 17:44:15 +0200

    handleChange function in HomeView (ZIP entry)

--- client/src/components/HomeView.js
+++ client/src/components/HomeView.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
 import './HomeView.css'
 
 function HomeView(){
+    // state for ZIP input. state/default value is empty string
+    const [zipInput, setZipInput] = useState("");
+
+    // handleChange function to update state of form input
+    function handleChange(event) {
+    let { name, value } = event.target; // set format of input
+    setZipInput((state) => ({ [name]: value }));
+  }
+    
+    
     // handleSubmit to send ZIP to App/update state
 
     return (
@@ -10,8 +20,11 @@ function HomeView(){
     <form>
         <label>
             Enter your ZIP code
-            <input type = "text"></input>
+            <input type = "text" onChange={(e) => handleChange(e)}></input>
         </label>
+        <button type="submit" class="btn btn-primary">
+            Search
+        </button>
     </form>     
     </div>
     )
