9536eb37e8b13bd53497e9a5625e4b0e51fba652
Zoe Laventhol <zoe.laventhol@gmail.com>
2022-11-02 19:36:27 +0100

    cleaned up App.css and Navbar.js

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -4,8 +4,10 @@ import { NavLink } from 'react-router-dom';
 function Navbar () {
     return (
         <div className="Navbar">
+            {/* Bootstrap code for nav bar */}
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <NavLink className="navbar-brand" to="/">
+                    {/* set California icon and text "MyEnviro" as HomeView link */}
                     <img 
                         src = "https://cdn-icons-png.flaticon.com/512/7906/7906437.png"
                         width="30" 
