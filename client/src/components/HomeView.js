c09ab064e995f5b7aec900d937b5e4e8056cb8ad
Zoe Laventhol <zoe.laventhol@gmail.com>
2022-11-02 19:56:03 +0100

    cleaned up HomeView.js and  HomeView.css

--- client/src/components/HomeView.js
+++ client/src/components/HomeView.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import { useNavigate } from 'react-router-dom';
 import './HomeView.css'
+// use React router navigation
+import { useNavigate } from 'react-router-dom';
 
 function HomeView(props){
     // state for ZIP input. state/default value is empty string
@@ -9,44 +10,46 @@ function HomeView(props){
     // use React router navigation
     const navigate = useNavigate();
 
-    // handleChange function to update state of form input
+    // handleChange function to update state of zipInput based on form input
     function handleChange(event) {
     let { value } = event.target; // set format of input
-    setZipInput((state) => (value));
+    setZipInput((state) => (value)); // update state to input value
     }
     
     
     // handleSubmit to send ZIP to App/update state
     function handleSubmit(submit) {
-        submit.preventDefault();
-        console.log("form submitted!");
-        // update state/pass to parent (App.js)
-        props.getLocalData(zipInput);
-        // go to indicators page
-        navigate("/indicators")
+        submit.preventDefault(); // prevent auto-reload
+        props.getLocalData(zipInput); // pass zipInput to parent (App.js), where it will be used to getLocalData
+        navigate("/indicators") // go to indicators page
     }
 
     return (
-    <div className="HomeView">
-        <div class="layer">
-            <div className="row">
-                <div className="col-8">
-                    <div className="row">
-                        <h1 id="zip-prompt">Enter your ZIP code</h1>
+        <div className="HomeView">
+            {/* add translucent layer over background image for better visibility */}
+            <div class="layer">
+                <div className="row">
+                    <div className="col-8">
+                        <div className="row">
+                            
+                            {/* header and descriptor */}
+                            <h1 id="zip-prompt">Enter your ZIP code</h1>
+                        </div>
+                        <div className="row">
+                            <h4>Learn about your air, water, home, and neighborhood - and how to protect yourself. Knowledge is power!</h4>
+                        </div>
+                        <div className="row">
+
+                            {/* ZIP input */}
+                            <form onSubmit={handleSubmit}>
+                                <input type = "text" onChange={(e) => handleChange(e)} placeholder="Enter ZIP here"></input>
+                                <button type="submit" class="btn btn-primary">Search</button>
+                            </form>
+                        </div>     
                     </div>
-                    <div className="row">
-                        <h4>Learn about your air, water, home, and neighborhood - and how to protect yourself. Knowledge is power!</h4>
-                    </div>
-                    <div className="row">
-                        <form onSubmit={handleSubmit}>
-                            <input type = "text" onChange={(e) => handleChange(e)} placeholder="Enter ZIP here"></input>
-                            <button type="submit" class="btn btn-primary">Search</button>
-                        </form>
-                    </div>     
                 </div>
             </div>
         </div>
-    </div>
     )
 }
 
