810cdfae20729be926378e3014107df236f03050
zoelaventhol <110328502+zoelaventhol@users.noreply.github.com>
2024-08-22 12:58:42 +0200

    corrected card titles

--- client/src/components/IndicatorCard.jsx
+++ client/src/components/IndicatorCard.jsx
@@ -5,7 +5,11 @@ function IndicatorCard({ indicator, envData, handleClick }) {
     // If data is null (aka less than 0), show no data message. In the enviro_data table, all null fields have been reset to -1 in order to differentiate them from 0-value data.
     // else if data is below hazard threshold, show good news
     // else, data is over hazard threshold, so show bad news
-    return envDataValue < 0 ? `${indicator.no_data}` : envDataValue < indicator.threshold ? `${indicator.good_news}` : `${indicator.bad_news}`;
+    return envDataValue < 0
+      ? `${indicator.no_data}`
+      : envDataValue < indicator.threshold
+      ? `${indicator.good_news}`
+      : `${indicator.bad_news}`;
   }
 
   // same logic/purpose as "news" function, but allows us to give conditional classes to pieces of our template (so we can style them in different colors)
@@ -17,12 +21,24 @@ function IndicatorCard({ indicator, envData, handleClick }) {
       : "bad-news";
   }
 
+  function title() {
+    return indicator.id === "air"
+      ? "Air"
+      : indicator.id === "water"
+      ? "Water"
+      : indicator.id === "haz_cleanups"
+      ? "Hazardous waste"
+      : indicator.id === "lead_paint"
+      ? "Lead paint"
+      : "";
+  }
+
   return (
     <div className="col">
       <div className="card" id="air-card">
         <img className="card-img-top" src={`${indicator.icon_url}`} alt="air" />
         <div className="card-body">
-          <h5 className="card-title">Air</h5>
+          <h5 className="card-title">{`${title()}`}</h5>
           <div className="card-text">
             {/* conditional class and message for air "news" - will return message and class (with different color) for "good_news", "bad_news", or "no_data", depending on the value for "air" in envData and the "threshold" in featIndicator. This is repeated on all cards */}
             <p className={conditionalClass()}>{`${news(
